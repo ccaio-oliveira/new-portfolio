b8212caab3cdce34c4de6f243242049ace46488f
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-08-26 11:31:22 -0300

    adicionando novo projeto

--- src/en/components/projects/projectJs/projectJs.jsx
+++ src/en/components/projects/projectJs/projectJs.jsx
@@ -4,12 +4,13 @@ import ProjectItem from '../projectItem/projectItem';
 const ProjectJs = () => {
     return(
         <>
-            <ProjectItem title="That's a postcode finder" src="../images/buscador-cep.jpeg" alt="Postcode finder home" href="https://grindelwald27.github.io/test-content/" />
-            <ProjectItem title="Ping Coming Soon" src="../images/ping-comming-soon.png" alt="Home ping coming soon" href="https://grindelwald27.github.io/ping-coming-soon-page-master/" />
-            <ProjectItem title="Intro Component Main" src="../images/intro-component.png" alt="Home intro component main" href="https://grindelwald27.github.io/intro-component-with-signup-form-master/" />
-            <ProjectItem title="Base Apparel coming soon" src="../images/base-apparel.png" alt="Base Apparel coming soon" href="https://grindelwald27.github.io/base-apparel-coming-soon-master/?email=" />
-            <ProjectItem title="Article preview component main" src="../images/article-preview.png" alt="Article preview component main" href="https://grindelwald27.github.io/article-preview-component-master/" />
-            <ProjectItem title="FAQ Acordion Card" src="../images/faq-card.png" alt="FAQ Acordion image" href="https://grindelwald27.github.io/faq-accordion-card-main/" />
+            <ProjectItem title="Interactive Card Details" src="../images/interactive-card.png" alt="Interactive Card image" href="https://ccaio-oliveira.github.io/interactive-card-details-form-main/" />
+            <ProjectItem title="That's a postcode finder" src="../images/buscador-cep.jpeg" alt="Postcode finder home" href="https://ccaio-oliveira.github.io/test-content/" />
+            <ProjectItem title="Ping Coming Soon" src="../images/ping-comming-soon.png" alt="Home ping coming soon" href="https://ccaio-oliveira.github.io/ping-coming-soon-page-master/" />
+            <ProjectItem title="Intro Component Main" src="../images/intro-component.png" alt="Home intro component main" href="https://ccaio-oliveira.github.io/intro-component-with-signup-form-master/" />
+            <ProjectItem title="Base Apparel coming soon" src="../images/base-apparel.png" alt="Base Apparel coming soon" href="https://ccaio-oliveira.github.io/base-apparel-coming-soon-master/?email=" />
+            <ProjectItem title="Article preview component main" src="../images/article-preview.png" alt="Article preview component main" href="https://ccaio-oliveira.github.io/article-preview-component-master/" />
+            <ProjectItem title="FAQ Acordion Card" src="../images/faq-card.png" alt="FAQ Acordion image" href="https://ccaio-oliveira.github.io/faq-accordion-card-main/" />
         </>
     )
 };
