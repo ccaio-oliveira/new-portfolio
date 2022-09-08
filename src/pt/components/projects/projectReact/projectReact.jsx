2ccec465385646ab72c290b6d5ee3e3038867447
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-09-07 22:08:16 -0300

    adicionando novo projeto

--- src/pt/components/projects/projectReact/projectReact.jsx
+++ src/pt/components/projects/projectReact/projectReact.jsx
@@ -6,6 +6,7 @@ const ProjectReact = () => {
         <>
             <ProjectItem title="Expenses Chart" src="./images/expenses-chart-component-main.png" alt="Expenses Chart" href="https://superlative-snickerdoodle-007a4f.netlify.app" />
             <ProjectItem title="Meu Portfolio" src="./images/portfolio.png" alt="Portfolio image" href="https://mydevelopernow.com" />
+            <ProjectItem title="Advice App" src="./images/advice-app.png" alt="Advice App" href="https://master--bejewelled-pasca-137756.netlify.app" />
         </>
     )
 }
