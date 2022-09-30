47e3e10be2d8d8e7a92c183ec08fd2987a21d195
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-09-30 08:47:23 -0300

    alterando usuario do instagram

--- src/en/components/contact/contact.jsx
+++ src/en/components/contact/contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
             <div className="buttons">
 
                 <ContactButton href="https://www.linkedin.com/in/caiooliveira82/" title="Linkedin" classIcon="fa-brands fa-linkedin" item="LinkedIn" />
-                <ContactButton href="https://www.instagram.com/ccaiooliveira27/" title="Instagram" classIcon="fa-brands fa-instagram" item="Instagram" />
+                <ContactButton href="https://www.instagram.com/caio.odev/" title="Instagram" classIcon="fa-brands fa-instagram" item="Instagram" />
                 <ContactButton href="mailto:ccaio.oliveira83@gnail.com" title="E-mail" classIcon="fa-solid fa-at" item="E-mail" />
                 <ContactButton href="https://github.com/ccaio-oliveira" title="GitHub" classIcon="fa-brands fa-github" item="GitHub" />
                 <ContactButton href="https://api.whatsapp.com/send?phone=5574998059407" title="WhatsApp" classIcon="fa-brands fa-whatsapp" item="WhatsApp" />
